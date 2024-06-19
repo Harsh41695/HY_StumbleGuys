10a6fe3cf01c8829f090004c2bc303a5d2c5eee0
HarshYadav161320 <102514854+Harsh41695@users.noreply.github.com>
2024-06-19 12:00:25 +0530

    Andriod Branch

--- Assets/HY_Assets/HY_Scripts/HY_InAppUpdate.cs
+++ Assets/HY_Assets/HY_Scripts/HY_InAppUpdate.cs
@@ -10,8 +10,7 @@ public class HY_InAppUpdate : MonoBehaviour
     //bool update = false;
     [SerializeField]
     private string playStoreUrlLink = "https://play.google.com/store/apps/details?id=com.mailtoalbatrossgamingstudios.PawSomeAdventure&pli=1";
-    private string appStroeUrlLink = "";
-    private string currentUrl;
+   
     void Awake()
     {
         if (Application.platform == RuntimePlatform.Android)
@@ -21,10 +20,7 @@ public class HY_InAppUpdate : MonoBehaviour
 
         StartCoroutine(CheckForPlayStoreUpdate());
 
-        if (Application.platform == RuntimePlatform.IPhonePlayer)
-        {
-            StartCoroutine(CheckForAppStoreUpdate());
-        }
+        
     }
     IEnumerator CheckForPlayStoreUpdate()
     {
@@ -56,41 +52,4 @@ public class HY_InAppUpdate : MonoBehaviour
         updatePanel.SetActive(false);
         //or Application Quit.
     }
-
-    /// if IPhone..
-    private IEnumerator CheckForAppStoreUpdate()
-    {
-        using (UnityWebRequest www = UnityWebRequest.Get(appStroeUrlLink))
-        {
-            yield return www.SendWebRequest();
-
-            if (www.result == UnityWebRequest.Result.Success)
-            {
-                string serverVersion = www.downloadHandler.text;
-                string currentVersion = Application.version;
-
-                if (serverVersion != currentVersion)
-                {
-                    // Update is available, prompt user to update
-                    PromptUpdate();
-                }
-                else
-                {
-                    Debug.Log("No update available.");
-                }
-            }
-            else
-            {
-                Debug.LogError("Error checking for update: " + www.error);
-            }
-        }
-    }
-    //Panel set active.
-    private void PromptUpdate()
-    {
-        // Implement your logic to prompt the user to update here
-        // This could involve showing a message to the user
-        Debug.Log("Update available. Please update the app.");
-    }
-
 }
